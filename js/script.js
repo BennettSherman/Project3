95483a9cb9d564714d364cff2d25225f9b92fb1a
BennettSherman <143107897+BennettSherman@users.noreply.github.com>
2023-11-15 09:52:36 -0600

    trying to fix map

--- js/script.js
+++ js/script.js
@@ -1,39 +1,12 @@
-var map;
-function initMap() {
+let map;
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.8741, lng: -87.6285},
-    zoom: 15
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
-
-  var school = new google.maps.Marker({
-    position: { lat: 41.8730, lng: -87.6279},
-    map: map
-  });
-
-  var home = new google.maps.Marker({
-    position: { lat: 41.868690, lng: -87.625380 },
-    map: map
-  });
-
-  var distPoints = [
-    { lat: 41.8730, lng: -87.6279 },
-    { lat: 41.87299780452928, lng: -87.62762881329594 },
-    { lat: 41.869100, lng: -87.627030 },
-    { lat: 41.869280, lng: -87.625870 },
-    { lat: 41.868690, lng: -87.625380 },
-  ];
-
-  var dist = new google.maps.Polyline({
-    path: distPoints,
-    geodesic: true,
-    strokeColor: "#FE5F55",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
-
-  dist.setMap(map);
-
 }
 
-window.initMap = initMap;
+initMap();
