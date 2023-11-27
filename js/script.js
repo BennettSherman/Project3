da605e0f8c44aad346f261ed8d956c5d8e87ac5d
BennettSherman <143107897+BennettSherman@users.noreply.github.com>
2023-11-26 21:39:47 -0600

    zooming in

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 41.809652513910486, lng: -87.62942911149604},
-    zoom: 8,
+    zoom: 15,
   });
 }
 
