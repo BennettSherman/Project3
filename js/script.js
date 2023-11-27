8aba77dca9f9a51e467251f733103dba911a74db
BennettSherman <143107897+BennettSherman@users.noreply.github.com>
2023-11-26 21:49:47 -0600

    zooming back in to 14

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 41.809652513910486, lng: -87.62942911149604},
-    zoom: 12,
+    zoom: 14,
   });
 
   var myStation = new google.maps.Marker({
